a798f4cd1ef63f10a2f261f3c4460ef018d8d95d
Esteban Laver <emlaver@us.ibm.com>
2017-06-19 11:28:02 -0400

    Testing suite: ClientSparkFunSuite for setting up, creating, and loading sample data from flat files to test databases. CloudantAllDocsDFSuite to test Spark data frames using the _all_docs endpoint. CloudantChangesDFSuite to test Spark data frames using the _changes endpoint. CloudantOptionSuite to verify Cloudant config options. CloudantSparkSQLSuite to test Spark SQL temp views.
    - Version 2.6.7 for jackson dependencies resolves "Incompatible Jackson version" during build
    - Cloudant set-up and database creation using cloudant-client library

---
+++ sql-cloudant/src/test/scala/org/apache/bahir/cloudant/TestUtils.scala
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.bahir.cloudant
+
+import java.io.File
+
+object TestUtils {
+  // List of test databases to create from JSON flat files
+  val testDatabasesList: List[String] = List(
+    "n_airportcodemapping",
+    "n_booking",
+    "n_customer",
+    "n_customersession",
+    "n_flight",
+    "n_flight2",
+    "n_flightsegment"
+  )
+
+  // Set CouchDB/Cloudant host, username and password for local testing
+  private val host = System.getenv("DB_HOST")
+  private val username = System.getenv("DB_USER")
+  private val password = System.getenv("DB_PASSWORD")
+  private val protocol = System.getenv("DB_PROTOCOL")
+
+  def deleteRecursively(file: File): Unit = {
+    if (file.isDirectory) {
+      file.listFiles.foreach(deleteRecursively)
+    }
+    if (file.exists && !file.delete) {
+      throw new Exception(s"Unable to delete ${file.getAbsolutePath}")
+    }
+  }
+
+  // default value is https for cloudant.com accounts, http for CouchDB
+  def getProtocol: String = {
+    if (protocol != null && !protocol.isEmpty) {
+      protocol
+    } else {
+      "https"
+    }
+  }
+
+  def getHost: String = {
+    if (host != null && !host.isEmpty) {
+      host
+    } else {
+      getUsername + ".cloudant.com"
+    }
+  }
+
+  def getUsername: String = {
+    username
+  }
+
+  def getPassword: String = {
+    password
+  }
+}
